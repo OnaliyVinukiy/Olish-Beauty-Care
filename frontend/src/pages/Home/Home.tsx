c34c83e9e39d94a89d7a53f060e30cab99a0a991
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-01-24 17:44:02 +0530

    Display carousel slider on home page

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,18 @@
+/*!
+ * Bloodline Blood Bank Management System
+ * Copyright (c) 2025 Onaliy Jayawardana
+ * All rights reserved.
+ *
+ * Unauthorized copying, modification, or distribution of this code is prohibited.
+ */
+import { CarouselSlider } from "./components/Carousel";
+
+const Home = () => {
+  return (
+    <div>
+      <CarouselSlider />
+    </div>
+  );
+};
+
+export default Home;
