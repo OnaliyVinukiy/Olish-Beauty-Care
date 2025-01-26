f02ed9c0256fd27abf757a22dadd3cd567475456
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-01-26 21:04:51 +0530

    Implement server

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,25 @@
-// server.js
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const authRoutes = require('./src/routes/authRoutes');
-
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const authRoutes = require("./src/routes/authRoutes");
+const productRoutes = require("./src/routes/productRoutes");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  "/uploads",
+  express.static(path.resolve(__dirname, "../../../frontend/src/uploads"))
+);
 
 // Use Auth routes
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
