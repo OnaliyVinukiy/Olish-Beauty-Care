11dddd14c70068493c1e3789feaf3c7e70d1ebd7
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-01-26 21:11:05 +0530

    Add copyright header

--- backend/src/controllers/featuredProductController.js
+++ backend/src/controllers/featuredProductController.js
@@ -1,3 +1,10 @@
+/*!
+ * Olish Beauty Care & Cosmetics Official Website
+ * Copyright (c) 2025 Olish Beauty Care & Cosmetics (Pvt) Ltd.
+ * All rights reserved.
+ *
+ * Unauthorized copying, modification, or distribution of this code is prohibited.
+ */
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
