dfb60d2ac7eb85a17c6e6efcf47451a483adbfe3
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-01-26 17:19:11 +0530

    Add copyright header

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -6,11 +6,12 @@
  * Unauthorized copying, modification, or distribution of this code is prohibited.
  */
 import { CarouselSlider } from "./components/Carousel";
-
+import FeaturedProducts from "./components/Featured";
 const Home = () => {
   return (
     <div>
       <CarouselSlider />
+      <FeaturedProducts />
     </div>
   );
 };
