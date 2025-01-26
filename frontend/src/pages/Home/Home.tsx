ea33c2424db223208f1e9adde0c7418be08e0e3b
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-01-26 17:21:30 +0530

    Implement backend

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 /*!
- * Bloodline Blood Bank Management System
- * Copyright (c) 2025 Onaliy Jayawardana
+ * Olish Beauty Care & Cosmetics Official Website
+ * Copyright (c) 2025 Olish Beauty Care & Cosmetics (Pvt) Ltd.
  * All rights reserved.
  *
  * Unauthorized copying, modification, or distribution of this code is prohibited.
