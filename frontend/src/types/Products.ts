662ad4669963fc6a8a3b030cd71a26a662f77ad7
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-01-26 21:08:01 +0530

    Add copyright header

--- frontend/src/types/Products.ts
+++ frontend/src/types/Products.ts
@@ -1,3 +1,10 @@
+/*!
+ * Olish Beauty Care & Cosmetics Official Website
+ * Copyright (c) 2025 Olish Beauty Care & Cosmetics (Pvt) Ltd.
+ * All rights reserved.
+ *
+ * Unauthorized copying, modification, or distribution of this code is prohibited.
+ */
 export interface Product {
     id: string;
     name: string;
