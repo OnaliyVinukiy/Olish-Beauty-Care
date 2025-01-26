cbac2a9b38820a6ac1a5a8ee555ef71a19cedb2f
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-01-26 21:05:58 +0530

    Add copyright header

--- frontend/src/pages/Admin/Dashboard.tsx
+++ frontend/src/pages/Admin/Dashboard.tsx
@@ -1,3 +1,10 @@
+/*!
+ * Olish Beauty Care & Cosmetics Official Website
+ * Copyright (c) 2025 Olish Beauty Care & Cosmetics (Pvt) Ltd.
+ * All rights reserved.
+ *
+ * Unauthorized copying, modification, or distribution of this code is prohibited.
+ */
 import React, { useState } from "react";
 import { Button, Table } from "flowbite-react";
 import AddFeatured from "./components/AddFeatured";
