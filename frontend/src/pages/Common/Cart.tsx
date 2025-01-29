3030469a83c2b4ef23532c6480b3e06d4d979f5a
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-01-29 15:05:57 +0530

    Implement Cart

---
+++ frontend/src/pages/Common/Cart.tsx
@@ -0,0 +1,227 @@
+import { useState, useEffect } from "react";
+import { Product } from "../../types/Products";
+import { Link } from "react-router-dom";
+
+export function CartPage() {
+  const [cart, setCart] = useState<Product[]>(() => {
+    return JSON.parse(localStorage.getItem("cart") || "[]");
+  });
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  const updateQuantity = (id: string, quantity: number) => {
+    setCart((prevCart) =>
+      prevCart.map((item) => (item.id === id ? { ...item, quantity } : item))
+    );
+  };
+
+  const removeFromCart = (id: string) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+  };
+
+  return (
+    <section className="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
+      <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
+        <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
+          Shopping Cart
+        </h2>
+
+        <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
+          <div className="mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl">
+            <div className="space-y-6">
+              {cart.map((product) => (
+                <div
+                  key={product.id}
+                  className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 md:p-6"
+                >
+                  <div className="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
+                    <a href="#" className="shrink-0 md:order-1">
+                      <img
+                        className=" inset-0 md:h-20 h-16 md:w-full object-cover dark:hidden"
+                        src={product.imageUrl}
+                        alt={product.name}
+                      />
+                      <img
+                        className="hidden md:h-20 md:w-20 object-cover dark:block"
+                        src={product.imageUrl}
+                        alt={product.name}
+                      />
+                    </a>
+
+                    <div className="flex items-center justify-between md:order-3 md:justify-end">
+                      <div className="flex items-center">
+                        <button
+                          type="button"
+                          onClick={() =>
+                            updateQuantity(
+                              product.id,
+                              (product.quantity || 1) - 1
+                            )
+                          }
+                          disabled={product.quantity === 1}
+                          className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+                        >
+                          <svg
+                            className="h-2.5 w-2.5 text-gray-900 dark:text-white"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 18 2"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M1 1h16"
+                            />
+                          </svg>
+                        </button>
+                        <input
+                          type="text"
+                          value={product.quantity}
+                          readOnly
+                          className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 dark:text-white"
+                        />
+                        <button
+                          type="button"
+                          onClick={() =>
+                            updateQuantity(
+                              product.id,
+                              (product.quantity || 1) + 1
+                            )
+                          }
+                          className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+                        >
+                          <svg
+                            className="h-2.5 w-2.5 text-gray-900 dark:text-white"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 18 18"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M9 1v16M1 9h16"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                      <div className="text-end md:order-4 md:w-32">
+                        <p className="text-base font-bold text-gray-900 dark:text-white">
+                          Rs. {product.price * (product.quantity || 1)}
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md">
+                      <p className="text-base font-medium text-gray-900 dark:text-white">
+                        {product.name}
+                      </p>
+                    </div>
+
+                    <button
+                      type="button"
+                      onClick={() => removeFromCart(product.id)}
+                      className="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500"
+                    >
+                      <svg
+                        className="me-1.5 h-5 w-5"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M6 18 17.94 6M18 18 6.06 6"
+                        />
+                      </svg>
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full">
+            <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
+              <p className="text-xl font-semibold text-gray-900 dark:text-white">
+                Order summary
+              </p>
+
+              <div className="space-y-4">
+                <div className="space-y-2">
+                  <dl className="flex items-center justify-between gap-4">
+                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                      Original price
+                    </dt>
+                    <dd className="text-base font-medium text-gray-900 dark:text-white">
+                      Rs.{" "}
+                      {cart.reduce(
+                        (acc, item) => acc + item.price * (item.quantity || 1),
+                        0
+                      )}
+                    </dd>
+                  </dl>
+
+                  <dl className="flex items-center justify-between gap-4">
+                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                      Shipping
+                    </dt>
+                    <dd className="text-base font-medium text-gray-900 dark:text-white">
+                      Rs. 300
+                    </dd>
+                  </dl>
+                </div>
+
+                <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700">
+                  <dt className="text-base font-bold text-gray-900 dark:text-white">
+                    Total
+                  </dt>
+                  <dd className="text-base font-bold text-gray-900 dark:text-white">
+                    Rs.{" "}
+                    {cart.reduce(
+                      (acc, item) => acc + item.price * (item.quantity || 1),
+                      0
+                    ) + 300}
+                  </dd>
+                </dl>
+              </div>
+
+              <Link
+                to="/checkout"
+                className="flex w-full items-center justify-center rounded-lg bg-cyan-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+              >
+                Proceed to Checkout
+              </Link>
+
+              <div className="flex items-center justify-center gap-2">
+                <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                  Need help?{" "}
+                  <Link
+                    to="/contact"
+                    className="text-cyan-700 hover:underline dark:text-cyan-500"
+                  >
+                    Contact us
+                  </Link>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
