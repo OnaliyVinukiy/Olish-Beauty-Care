8560f277d6154bed201b2376ecd8370d0262a53b
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-01-30 12:20:40 +0530

    Display new sections in home page

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -5,13 +5,48 @@
  *
  * Unauthorized copying, modification, or distribution of this code is prohibited.
  */
-import { CarouselSlider } from "./components/Carousel";
+
 import { FeaturedProducts } from "./components/Featured";
+import { TypewriterEffect } from "./components/TextEffect";
+import { ParaEffect } from "./components/ParaEffect";
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    console.log("Loading state in Home:", loading);
+  }, [loading]);
+
   return (
     <div>
-      <CarouselSlider />
-      <FeaturedProducts />
+      {loading && (
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="h-12 w-12 animate-spin rounded-full border-4 border-pink-400 border-t-transparent"></div>
+        </div>
+      )}
+      <FeaturedProducts setLoading={setLoading} />
+      {!loading && (
+        <>
+          <section className="mb-12 bg-pink-50 py-8 md:ml-20 md:mr-20 ml-8 mr-8 antialiased dark:bg-gray-900 md:py-16 rounded-lg">
+            <TypewriterEffect
+              words={[
+                { text: "Beauty" },
+                { text: "&" },
+                { text: "Wellness" },
+                { text: "In" },
+                { text: "Every" },
+                { text: "Drop!" },
+              ]}
+            />
+            <ParaEffect
+              words={
+                "Experience the perfect blend of nature and science with Olish Beauty Care. Our carefully crafted formulas nourish your skin, enhance your natural glow, and bring out the beauty in every drop. Elevate your skincare routine with the essence of purity and wellness."
+              }
+            />
+          </section>
+        </>
+      )}
     </div>
   );
 };
